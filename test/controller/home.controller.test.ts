27b69fb709765936c0dfd7f188fbc6a32d922ea2
xufan.zhang <sunshinezxf@hotmail.com>
2024-07-23 14:53:04 +0800

    去除npm run test

--- test/controller/home.controller.test.ts
+++
@@ -1,9 +0,0 @@
-import {createApp, close, createHttpRequest} from '@midwayjs/mock';
-import {Framework} from '@midwayjs/koa';
-
-describe('test/controller/home.controller.test.ts', async () => {
-    const app = await createApp<Framework>();
-    const result = await createHttpRequest(app).get('/');
-    expect(result.status).toBe(200);
-    await close(app);
-});
